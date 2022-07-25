8c25f93869e50c595311ec17dd0fb42b5c9e2258
Antun <antun.jerkovic@google.com>
2022-07-25 13:20:28 +0200

    d1

--- 01_IntroToPython/005_quiz_Variables_and_printing.py
+++ 01_IntroToPython/005_quiz_Variables_and_printing.py
@@ -1,93 +1,30 @@
-# Question 1
-# Which of these is the correct way to define (assign a value to) a variable in Python?
-#
-# 1
-#
-# x:25
-#
-# 2
-#
-# x = 25
-#
-# 3
-#
-# x(25)
-#
-# 4
-#
-# print(25)
-#
-# Correct Answer
-# 2
-# x = 25
-#
-# Question 2
-# What would the output be if you run the code below?
-#
-# x = 25
-# print(x)
-# x = 30
-# print(x)
-# 1
-#
-# 25
-#
-# 30
-#
-# 2
-#
-# 25
-#
-# 25
-#
-# 3
-#
-# This would give you an error because x already has a value when we do x = 30.
-#
-# Correct Answer
-# 1
-# 25
-#
-# 30
-#
-# Question 3
-# Which of these is not a valid Python variable name?
-#
-# 1
-#
-# myVariable
-#
-# 2
-#
-# my_variable
-#
-# 3
-#
-# _var
-#
-# 4
-#
-# 1st_variable
-#
-# Correct Answer
-# 4
-# 1st_variable
-#
-# Question 4
-# Why would you write a variable name in all uppercase (all capital letters) in Python?
-#
-# 1
-#
-# There's no reason to ever do this in Python. It's the wrong style.
-#
-# 2
-#
-# To signal that you don't intend for this variable's value to change.
-#
-# 3
-#
-# When defining important values that will be used throughout the entire code.
-#
-# Correct Answer
-# 2
-# To signal that you don't intend for this variable's value to change.
+
+# * Question 1: Which of these is the correct way to define (assign a value to) a variable in Python?
+# 1) x:25
+# 2) x = 25
+# 3) x(25)
+# 4) print(25)
+# * Correct Answer: 2) x = 25
+
+# * Question 2: What would the output be if you run the code below?
+# ? x = 25
+# ? print(x)
+# ? x = 30
+# ? print(x)
+# 1) 25 30
+# 2) 25 25
+# 3) This would give you an error because x already has a value when we do x = 30.
+# * Correct Answer: 1) 25 30
+
+# * Question 3: Which of these is not a valid Python variable name?
+# 1) myVariable
+# 2) my_variable
+# 3) _var
+# 4) 1st_variable
+# * Correct Answer: 4) 1st_variable
+
+# * Question 4: Why would you write a variable name in all uppercase (all capital letters) in Python?
+# 1) There's no reason to ever do this in Python. It's the wrong style.
+# 2) To signal that you don't intend for this variable's value to change.
+# 3) When defining important values that will be used throughout the entire code.
+# * Correct Answer: 2) To signal that you don't intend for this variable's value to change.
